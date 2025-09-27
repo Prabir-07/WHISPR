bcbe9134a6b84eab6d64b8151cea13eafbb9bf1c
Prabir <prabirkumarpanda90@gmail.com>
2025-09-27 15:55:48 +0530

    Implement multer and error handle middlewares

--- chat-app-backend/src/middlewares/error.middleware.js
+++ chat-app-backend/src/middlewares/error.middleware.js
@@ -1,1 +1,20 @@
-import {ApiError} from '../'
+import { ApiError } from "../utils/ApiError.js";
+import { StatusCodes } from "http-status-codes";
+
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+      errors: err.errors,
+    });
+  }
+
+  console.error("UNHANDLED ERROR: ", err);
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+};
+
+export { errorHandler };
